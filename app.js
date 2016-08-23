4579c50a7f9e36ed54e7125c10a5a05fd2655f75
Taylor Welling <tw3080@gmail.com>
2016-08-23 15:15:00 -0700

    added scope variable 'isEditable' to determine whether or not the text can be edited; added 'editContent' function to change the value of isEditable to 'true'

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var app = angular.module('editableItemApp', []);
 app.directive('makeEditable', function() {
     return {
         restrict: 'A',
-        templateUrl: 'editable-item-template.html'
+        templateUrl: 'editable-item-template.html',
+        link: function($scope, element, attrs) {
+            $scope.isEditable = false;
+            $scope.editContent = function() {
+                $scope.isEditable = true;
+            };
+        }
     };
 });
